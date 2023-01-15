88eba467d5aa4c9616d1d65bcaa08d1c4003351a
Kacper Adler <contact@kacperadler.com>
2023-01-15 16:58:49 +0100

    feat: Add google analytics

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -1,9 +1,22 @@
 import { Html, Head, Main, NextScript } from 'next/document';
+import Script from 'next/script';
+import { ENV } from 'shared/config/env';
 
 export default function Document() {
   return (
     <Html lang="en" className="dark:bg-slate-900">
-      <Head />
+      <Head>
+        <Script async src={`https://www.googletagmanager.com/gtag/js?id=${ENV.G_TAG}`} strategy="afterInteractive" />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+
+            gtag('config', '${ENV.G_TAG}');
+          `}
+        </Script>
+      </Head>
       <body>
         <Main />
         <NextScript />
---
+++ src/shared/config/env.ts
@@ -0,0 +1,3 @@
+export const ENV = {
+  G_TAG: process.env.G_TAG ?? '',
+};
